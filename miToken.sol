8d619472890995babcbab8b2a7a3c09880e81d5c
z <144504951+deynasaur@users.noreply.github.com>
2024-11-29 17:17:46 +0800

    Update miToken.sol

--- miToken.sol
+++ miToken.sol
@@ -2,16 +2,19 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract miToken is ERC20, ERC20Burnable, Ownable {
+contract miToken is ERC20, Ownable {
 
     constructor(address owner) ERC20("Francheska", "ESKA") Ownable(owner)
     {}
 
     function mint(address _to, uint _amount) public onlyOwner {
         _mint(_to, _amount);
+    }
+
+    function burn(uint _amount) public {
+        _burn(msg.sender, _amount);
     }
 
     function transfer(address _to, uint _amount) public override returns (bool) {
